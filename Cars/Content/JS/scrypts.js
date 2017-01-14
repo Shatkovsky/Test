870fa138e12d5ee81fc0124e0b8ab06b88b89831
Shatkovsky <shatkovsky.k@gmail.com>
2017-01-14 10:23:07 +0200

    test

---
+++ Cars/Content/JS/scrypts.js
@@ -0,0 +1,50 @@
+/*Практика JavaScript*/
+function SetIMG(selected_url) {
+    var item = document.getElementById("Preview");
+    item.src = selected_url;
+}
+/*Практика jQuery*/
+$(document).ready(function () {
+    $('#header').fadeTo(2000, 0.3, function () {
+        $('html, body').animate({ scrollTop: $('#content').offset().top }, 2000);
+    });
+});
+
+/*Ajax jQuery*/
+$(document).on('submit', 'form', function () {
+
+    var UserName = $('input[name=Name]').val();
+    var UserTel = $('input[name=Tel]').val();
+    var Car = $('select').val();
+    //Валидация введенных данных.
+    //Проверяем имя
+    if (UserName.length > 0) {
+        //Проверяем номер телефона
+        var regular = /^\8-[0-9]{3}-[0-9]{3}-[0-9]{4}/;
+        if (regular.test(UserTel) == true) {
+            alert('Phone is OK!');
+            SubmitForm(UserName, UserTel, Car);
+        }
+        else {
+            alert("Wrong phone number");
+        }
+    }
+    else {
+        alert("You fogot enter your name");
+    }
+});
+
+/*Ajax jQuery*/
+function SubmitForm(name, tel, car) {
+    $.ajax({
+        method: "POST",
+        url: "/Home/Form",
+        data: { Name: name, Tel: tel, Car: car }
+    })
+   .done(function (context) {
+       alert(context);
+   }).fail(function () {
+       alert("Error when sending data");
+   });
+}
+
---
+++ Cars/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Cars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cars.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private Models.ShopDBEntities db = new Models.ShopDBEntities();
+        public ActionResult Index()
+        {
+            var Items = db.Cars;
+            return View(Items);
+        }
+
+        public ActionResult CarPage(int item_id)
+        {
+            var Item = db.Cars.FirstOrDefault(x => x.Id == item_id);
+            if (Item == null)
+            {
+                return Content("<h1>Page not found. Incorrect input data</h1>");
+            }
+            return View(Item);
+        }
+
+        [ChildActionOnly]
+        public ActionResult Nav()
+        {
+            var items = db.Cars;
+            string str = "";
+            foreach (var item in items)
+            {
+                str += "<li><a href='/Home/CarPage/?item_id=" + item.Id + "' title='" + item.Title + "'>" + item.Title + "</a></li>";
+            }
+            return Content(str);
+        }
+
+        [HttpGet]
+        public ActionResult Form(int item_id = 0)
+        {
+            ViewBag.Item = item_id;
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public string FormOptions(int item_id = 0)
+        {
+            var Items = db.Cars;
+            string str = "";
+            foreach (var item in Items)
+            {
+                if (item.Id == item_id)
+                {
+                    str += "<option value=" + item.Id + " selected>" + item.Title + "</option>";
+                }
+                else
+                {
+                    str += "<option value=" + item.Id + ">" + item.Title + "</option>";
+                }
+            }
+            return str;
+        }
+
+        [HttpPost]
+        public string Form(string Name, string Tel, int Car)
+        {
+            string outStr = "";
+            Order order = new Order
+            {
+                UserName = Name,
+                UserTel = Tel,
+                CarId = Car,
+                Status = "Created"
+            };
+            db.Orders.Add(order);
+            db.SaveChanges();
+            outStr = "Your request for buy new car " + db.Cars.FirstOrDefault(x => x.Id == Car).Title + " created!";
+            return outStr;
+        }
+    }
+}
